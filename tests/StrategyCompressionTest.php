6db06bb092574d1232c58976ec801a578082e77a
Stanislaw Molitorys <s.molitorys@gmail.com>
2018-02-21 20:33:53 +0100

    Add more tests to Strategy Compression classes

--- tests/StrategyCompressionTest.php
+++ tests/StrategyCompressionTest.php
@@ -8,30 +8,40 @@ use DesignPatterns\Strategy\Compression\RarCompression;
 
 class StrategyComporessionTest extends TestCase
 {
-    private $compressionSystem;
-
-    public function setUp()
-    {
-        $this->compressionSystem = new CompressionSystem();
-    }
-
     /** @test */
     public function it_can_compress_zip_files()
     {
         $filePath = 'plik-archiwalny-1.ods';
 
-        $this->compressionSystem->setCompressionEngine(new ZipCompression());
+        $zipCompressor = new ZipCompression();
 
-        $this->assertEquals('Kompresuje plik '.$filePath.' do formatu ZIP', $this->compressionSystem->compress($filePath));
+        $this->assertEquals('Kompresuje plik '.$filePath.' do formatu ZIP', $zipCompressor->compressFile($filePath));
     }
 
     /** @test */
     public function it_can_compress_rar_files()
     {
         $filePath = 'plik-archiwalny-2.ods';
- 
-        $this->compressionSystem->setCompressionEngine(new RarCompression());
- 
-        $this->assertEquals('Kompresuje plik '.$filePath.' do formatu RAR', $this->compressionSystem->compress($filePath));
+
+        $rarCompressor = new RarCompression();
+
+        $this->assertEquals('Kompresuje plik '.$filePath.' do formatu RAR', $rarCompressor->compressFile($filePath));
+    }
+
+    /** @test */
+    public function compression_system_can_compress_files()
+    {
+        $filePath1 = 'plik-archiwalny-1.ods';
+        $filePath2 = 'plik-archiwalny-2.ods';
+
+        $compressionSystem = new CompressionSystem();
+
+        $zipCompressor = new ZipCompression();
+        $compressionSystem->setCompressionEngine($zipCompressor);
+        $this->assertEquals('Kompresuje plik '.$filePath1.' do formatu ZIP', $compressionSystem->compress($filePath1));
+        
+        $rarCompressor = new RarCompression();
+        $compressionSystem->setCompressionEngine($rarCompressor);
+        $this->assertEquals('Kompresuje plik '.$filePath2.' do formatu RAR', $compressionSystem->compress($filePath2));
     }
 }
