aa9dce8a5c78c97ab793ad4013ee0e0e320fbbf5
Stanisław Molitorys <s.molitorys@gmail.com>
2023-07-28 13:20:18 +0200

    Add Bridge example

--- src/index.php
+++ src/index.php
@@ -1,22 +1,58 @@
 <?php
 
 $clients = [
-    'Adapter - Robot' => 'Adapter/Robot/client.php',
-    'Bridge - Shape' => 'Bridge/Shape/client.php',
-    'Builder - Robot' => 'Builder/Robot/client.php',
-    'Chain of Responsibility - Numbers' => 'ChainOfResponsibility/Numbers/client.php',
-    'Command - Electronic Device' => 'Command/ElectronicDevice/client.php',
-    'Decorator - Sandwich' => 'Decorator/Sandwich/client.php',
-    'Facade - Bank Account' => 'Facade/BankAccount/client.php',
-    'Flyweight - Shapes' => 'Flyweight/Shapes/client.php',
-    'Model View Controller (MVC) - Calcilator' => 'MVC/Calculator/client.php',
-    'Observer - Stock' => 'Observer/Stock/client.php',
-    'Singleton - Database Manager' => 'Singleton/DatabaseManager/client.php',
-    'Strategy - Compression' => 'Strategy/Compression/client.php',
-    'Template Method - Sandwich' => 'TemplateMethod/Sandwich/client.php',
+    'Adapter' => [
+        'Robot' => 'Adapter/Robot/client.php'
+    ],
+    'Bridge' => [
+        'Shape' => 'Bridge/Shape/client.php',
+        'Entertainment Device' => 'Bridge/EntertainmentDevice/client.php'
+    ],
+    'Builder' => [
+        'Robot' => 'Builder/Robot/client.php'
+    ],
+    'Chain of Responsibility' => [
+        'Numbers' => 'ChainOfResponsibility/Numbers/client.php'
+    ],
+    'Command' => [
+        'Electronic Device' => 'Command/ElectronicDevice/client.php'
+    ],
+    'Decorator' => [
+        'Sandwich' => 'Decorator/Sandwich/client.php'
+    ],
+    'Facade' => [
+        'Bank Account' => 'Facade/BankAccount/client.php'
+    ],
+    'Flyweight' => [
+        'Shapes' => 'Flyweight/Shapes/client.php'
+    ] ,
+    'Model View Controller (MVC)' => [
+        'Calculator' => 'MVC/Calculator/client.php'
+    ],
+    'Observer' => [
+        'Stock' => 'Observer/Stock/client.php'
+    ] ,
+    'Singleton' => [
+        'Database Manager' => 'Singleton/DatabaseManager/client.php'
+    ] ,
+    'Strategy' => [
+        'Compression' => 'Strategy/Compression/client.php'
+    ],
+    'Template Method' => [
+        'Sandwich' => 'TemplateMethod/Sandwich/client.php'
+    ]
 ];
 
 echo '<h1>Design Patterns</h1>';
-foreach ($clients as $patternName => $client) {
-    echo '<a target="_blank" href="'.$client.'">'.$patternName.'</a><br>';
+echo '<ul>';
+foreach ($clients as $patternName => $examples) {
+    echo '<li>';
+        echo '<h3>'.$patternName.'</h3>';
+        echo '<ul>';
+        foreach ($examples as $name => $client) {
+            echo '<li><a target="_blank" href="'.$client.'">'.$name.'</a></li>';
+        }
+        echo '</ul>';
+    echo '</li>';
 }
+echo '</ul>';
